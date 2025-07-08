acbb61d41c825d059873b5599a3805c04fe2573a
JPgitidaho <juanichile2015@gmail.com>
2025-07-07 20:46:08 -0400

    actualization

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,4 @@
-import { useEffect, useState } from "react";
-import { searchMealsByName } from "../services/api";
-import MealCard from "../components/MealCard";
-import SearchBar from "../components/SearchBar";
-
-export default function Home() {
-  const [meals, setMeals] = useState([]);
-
-  useEffect(() => {
-    searchMealsByName("chicken").then(setMeals);
-  }, []);
-
-  return (
-    <div>
-      <SearchBar onSearch={setMeals} />
-      <div style={{ display: "flex", flexWrap: "wrap", gap: "1rem" }}>
-        {meals?.map(meal => (
-          <MealCard key={meal.idMeal} meal={meal} />
-        ))}
-      </div>
-    </div>
-  );
-}
+import Header from '../header/Header'
+import Hero from '../header/Hero'
+import RandomRecipes from '../cards/RandomRecipes'
+import Footer from '../components/Footer'
