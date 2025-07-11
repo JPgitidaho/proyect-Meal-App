66d9b78bd4f3c7605678d740b48018745d8e5285
JPgitidaho <juanichile2015@gmail.com>
2025-07-11 13:47:02 -0400

    actualization

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -3,16 +3,16 @@ import HamburgerMenu from "./HamburgerMenu";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 w-full h-[7vh] z-50 bg-white py-10 px-6 md:px-20">
+    <header className="fixed top-0 left-0 w-full h-[7vh] z-50 bg-white py-10 px-6 md:px-15">
       <nav className="flex justify-between items-center h-full max-w-7xl mx-auto ">
         <Link to="/">
-          <img src="/assets/logo.svg" alt="Logo" className="w-60 h-8" />
+          <img src="/assets/logo.svg" alt="Logo" className="w-60 h-8 ml-10" />
         </Link>
 
-        <div className="hidden md:flex gap-6 text-[#586833] font-medium">
+        <div className="hidden md:flex gap-6 md:mx-14 text-[#586833] font-medium">
           <Link to="/">Inicio</Link>
-          <Link to="/todas-categorias">All Categories</Link>
-          <Link to="/todas-localizaciones">All Localizations</Link>
+          <Link to="/todas-categorias">Categories</Link>
+          <Link to="/todas-localizaciones">Countrys</Link>
         </div>
 
         <div className="md:hidden">
